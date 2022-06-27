db46208fa762892871a85ae0334a50efcf8cb722
Jordan Estácio <jordan140997@gmail.com>
2022-06-27 20:30:04 -0300

    verificação da faixa de cep

--- buscaCep.js
+++ buscaCep.js
@@ -3,8 +3,15 @@ requestData.open("GET", "/ceps.json");
 requestData.responseType = "json";
 requestData.send();
 
+const cepClient = 69900000;
+// const cepClient = 20000001;
+
 requestData.onload = () => {
-    let wrapper = requestData.response;
-    console.log(wrapper)
-    
-}
+	let wrapper = requestData.response;
+	for (const i in wrapper) {
+        if(cepClient >= wrapper[i].cepInicial && cepClient <= wrapper[i].cepFinal){
+            window.location.href = wrapper[i].url
+            return false
+        }
+	}
+};
